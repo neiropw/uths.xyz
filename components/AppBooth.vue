59c874132bf10e8c9906563cb0b4bf2acbacc83a
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-17 23:19:16 +0900

    #2 簡単なページ作成

--- components/AppBooth.vue
+++ components/AppBooth.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="search-card-wrap" :booth="booth">
     <a class="search-card" href="#">
-      <div class="tags"><span class="label label-category">{{ categoryToName(booth.category) }}</span> <span class="label label-person">{{ booth.person.name }}</span> <span class="label label-place">{{ booth.place.name }}</span></div>
-      <div class="title">{{ booth.name }}</div>
-      <div class="content">{{ booth.content }}</div>
+      <div class="tags">
+        <span class="label label-category">{{ categoryToName(booth.category) }}</span>
+        <span class="label label-person">{{ booth.person.name }}</span>
+        <span class="label label-place">{{ placeCategoryToName(booth.place.category) }}{{ booth.place.floor !== null ? ` ${booth.place.floor}階` : '' }}</span>
+      </div>
+      <div class="card-content">
+        <div class="title">{{ booth.name }}</div>
+        <div class="content">{{ booth.content }}</div>
+      </div>
     </a>
   </div>
 </template>
@@ -14,12 +20,25 @@ export default {
   methods: {
     categoryToName(category) {
       switch(category) {
-        case 'SEE':
-          return '見る'
-        case 'EAT':
-          return '食べる'
-        case 'PLAY':
-          return '体験する'
+        case 'SEE': return '見る'
+        case 'EAT': return '食べる'
+        case 'PLAY': return '体験する'
+        default: return category
+      }
+    },
+    placeCategoryToName(placeCategory) {
+      switch(placeCategory) {
+        case 'MS': return '機械システム系 実習棟'
+        case 'EI': return '電気情報システム系 実習棟'
+        case 'BD': return '建築システム系 実習棟'
+        case 'ES': return '環境建設システム系 実習棟'
+        case 'RC': return '普通教室棟'
+        case 'SC': return '管理・特別教室棟'
+        case 'IC': return 'インタラクティブコート(中庭)'
+        case 'SY': return '校庭'
+        case 'AR1': return '第一アリーナ'
+        case 'AR2': return '第二アリーナ'
+        default: return placeCategory
       }
     }
   }
@@ -40,8 +59,8 @@ export default {
   text-decoration: none;
   color: #000;
 }
-  .search-card .title {
-    padding: 5px;
+  .search-card .card-content { margin: 5px; }
+  .search-card .card-content .title {
     font-size: 1.75em;
   }
 .label {
--- layouts/booth.vue
+++ layouts/booth.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <header class="concept">
-      <div class="card">
-        <div class="title">宇工祭2018</div>
-        <div class="content">
-          2018.11.03 (祝)<br />
-          栃木県立宇都宮工業高等学校
-        </div>
-        <div class="theme">まだテーマすら発表されてない<br />多分直前にならないとわからないやつだこれ</div>
-      </div>
-    </header>
     <nav class="nav">
       <ul>
         <li><a href="#" class="ms">開催概要</a></li>
@@ -39,20 +29,6 @@ html {
 small {
   font-size: 1rem;
 }
-.concept {
-  background: rgba(0, 0, 0, 0.5);
-  text-align: center;
-}
-.concept .card {
-  display: inline-block;
-  margin: 30px; padding: 30px;
-  background: #fff;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
-}
-  .concept .card .title { font-weight: bold; font-size: 2em; }
-  .concept .card .content { color: rgb(89, 89, 89); }
-  .concept .card .theme { padding: 0; padding-top: 20px; font-size: 1.75em; }
-
 .nav {
   background: #fff;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
