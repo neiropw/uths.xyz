f3a416a7447f532908bd7a91b47f1df2c13d8673
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-17 12:10:42 +0900

    #1 コンポーネント化

--- pages/index-2.vue
+++ pages/index-2.vue
@@ -43,35 +43,17 @@
     </form>
     <section class="search-result">
 
-      <div class="search-card-wrap">
-        <a class="search-card" href="#">
-          <div class="tags"><span>体験する</span> <span>電子情報科 2年</span> <span>電気情報システム系 実習棟 2F</span></div>
-          <div class="title">LANケーブル作成体験</div>
-          <div class="content">ここに各ブースの説明文が入ります。大体2行ぐらいで収まるはずです。</div>
-        </a>
-      </div>
-      <div class="search-card-wrap">
-        <a class="search-card" href="#">
-          <div class="tags"><span>体験する</span> <span>電子情報科 2年</span> <span>電気情報システム系 実習棟 2F</span></div>
-          <div class="title">LANケーブル作成体験</div>
-          <div class="content">ここに各ブースの説明文が入ります。大体2行ぐらいで収まるはずです。</div>
-        </a>
-      </div>
-      <div class="search-card-wrap">
-        <a class="search-card" href="#">
-          <div class="tags"><span>体験する</span> <span>電子情報科 2年</span> <span>電気情報システム系 実習棟 2F</span></div>
-          <div class="title">LANケーブル作成体験</div>
-          <div class="content">ここに各ブースの説明文が入ります。大体2行ぐらいで収まるはずです。</div>
-        </a>
-      </div>
+      <app-booth />
 
     </section>
   </main>
 </template>
 
 <script>
+import AppBooth from '../components/AppBooth'
 
 export default {
+  components: { AppBooth },
   layout: 'home'
 }
 
