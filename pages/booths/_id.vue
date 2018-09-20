2d7151e8fb93e38989fe7affcc1a1880652b16ee
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-20 22:06:15 +0900

    #1 デザイン作成

--- pages/booths/_id.vue
+++ pages/booths/_id.vue
@@ -1,26 +1,29 @@
 <template>
   <main class="container">
-    <h1><span>体験する</span> LANケーブル制作体験</h1>
-    <p>ここに各ブースの説明文が入ります。大体２行ぐらいで収まるはずです。</p>
-    <h2>このブースの詳しい説明</h2>
+    <div class="booth-title">
+      <span class="label label-category">体験する</span>
+      <h1 class="booth-name">LANケーブル制作体験</h1>
+    </div>
+    <p class="booth-detail">
+      ここに各ブースの説明文が入ります。大体２行ぐらいで収まるはずです。
+    </p>
+    <h2 class="booth-info">このブースの詳しい説明</h2>
     <div class="row">
       <div class="column">
-        <table>
+        <table class="info">
           <tr>
             <th>担当</th>
-            <td><span>電子情報科 2年</span></td>
+            <td><span class="label label-person">電子情報科 2年</span></td>
           </tr>
           <tr>
             <th>場所</th>
             <td>
-              <span>電気情報システム系 実習棟</span>
-              <span>2階</span>
-              <span>光ファイバー実習室</span>
+              <span class="label label-place">電気情報システム系 実習棟 2階 光ファイバー実習室</span>
             </td>
           </tr>
           <tr>
             <th>時間</th>
-            <td><span>随時</span></td>
+            <td><span class="label label-time">随時</span></td>
           </tr>
         </table>
       </div>
@@ -70,10 +73,43 @@ export default {
 .container { }
 .row {
   overflow: hidden;
+  padding: 20px;
 }
+  .row table { font-size: 1.25em; }
+  .row table th, .row table td { padding: 5px; }
+  .row table th { font-weight: normal; color: #888; }
+  .row table td li::before { content: '・'; }
+  .row table td li { list-style: none; }
 .column {
   width: 50%;
   float: left;
+}
+.label {
+  display: inline-block;
+  vertical-align: middle;
+  margin: 0 5px;
+  padding: 5px;
+  font-size: 0.95em;
+  font-weight: bold;
+}
+  .label-category { margin-left: 0; background: rgb(255, 80, 80); color: #fff; }
+  .label-person { background: rgb(255, 153, 51); color: #fff; }
+  .label-place { background: rgb(91, 155, 213); color: #fff; }
+  .label-time { background: rgb(112, 173, 71); color: #fff; }
+.booth-title { border-bottom: 1px solid rgb(255, 80, 80); }
+.booth-name {
+  display: inline-block;
+  vertical-align: middle;
+  font-weight: normal;
+  color: rgb(255, 80, 80);
+}
+.booth-info {
+  font-weight: bold;
+  color: rgb(255, 153, 51);
+  border-bottom: 1px solid rgb(255, 153, 51);
+}
+.booth-detail {
+  padding: 20px;
 }
 </style>
 
